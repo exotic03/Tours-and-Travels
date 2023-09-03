9469bdd0f97b548646ea3760f9781ead4eb873fe
Sourav Saha <saha83787@gmail.com>
2023-09-03 14:20:58 +0530

    animation added

--- script.js
+++ script.js
@@ -1,12 +1,103 @@
 gsap.to("nav",{
-    backgroundColor:"#e2eafc",
+    backgroundColor:"#b6ccfe",
+    boxShadow:"1px 1px 5px #011627",
     duration:0.5,
     scrollTrigger:{
         trigger:"nav",
         scroller:"body",
-        markers:true,
         start:"top -5%",
         end:"top -6%",
+        scrub:2
+    }
+});
+gsap.from(".c1",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:60,
+    scrollTrigger:{
+        trigger:".c1",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:2
+    }
+});
+gsap.from(".c2",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".c2",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:2
+    }
+});
+gsap.from(".c3",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:60,
+    scrollTrigger:{
+        trigger:".c3",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:2
+    }
+});
+gsap.from(".c4",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".c4",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:2
+    }
+});
+gsap.from(".c5",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:60,
+    scrollTrigger:{
+        trigger:".c5",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:2
+    }
+});
+gsap.from(".c6",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:-60,
+    scrollTrigger:{
+        trigger:".c6",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
+        scrub:2
+    }
+});
+gsap.from(".c7",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+    x:60,
+    scrollTrigger:{
+        trigger:".c7",
+        scroller:"body",
+        start:"top 80%",
+        end:"top 90%",
         scrub:2
     }
 });
