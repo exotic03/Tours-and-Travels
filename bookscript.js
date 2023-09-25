83caa59e43eab7dcae9363d16f28763f51871cfb
Sourav Saha <saha83787@gmail.com>
2023-09-25 22:32:37 +0530

    booking page created

---
+++ bookscript.js
@@ -0,0 +1,18 @@
+gsap.to("nav",{
+    backgroundColor:"#b6ccfe",
+    boxShadow:"1px 1px 5px #011627",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"nav",
+        scroller:"body",
+        start:"top -5%",
+        end:"top -6%",
+        scrub:2
+    }
+});
+
+let togglebtn=document.getElementById("toggle-btn");
+let navlink=document.getElementById("navlinks");
+togglebtn.addEventListener('click',()=>{
+    navlink.classList.toggle('active');
+});
