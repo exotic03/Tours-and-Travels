4361da72b3382cf292b7da3498558307df617e7b
Sourav Saha <saha83787@gmail.com>
2023-09-27 22:35:05 +0530

    traveller add feature added

--- bookscript.js
+++ bookscript.js
@@ -16,3 +16,38 @@ let navlink=document.getElementById("navlinks");
 togglebtn.addEventListener('click',()=>{
     navlink.classList.toggle('active');
 });
+
+window.onload=()=>{
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    if(month<10){
+        month='0'+month;
+    }
+    if(day<10){
+        day='0'+day;
+    }
+    
+    var maxDate = year + '-' + month + '-' + day;
+
+    var inputDate = document.getElementById('date');
+    inputDate.setAttribute('min', maxDate);
+}
+
+var detailsForm=document.getElementById("detailsform");
+
+function addTraveller(){
+    var nameBox=document.createElement("input");
+    var ageBox=document.createElement("input");
+    nameBox.type="text";
+    nameBox.name="tname[]";
+    nameBox.placeholder="Add Traveller";
+    nameBox.className="inputBox";
+    ageBox.type="text";
+    ageBox.name="tage[]";
+    ageBox.placeholder="Add Traveller Age";
+    ageBox.className="inputBox";
+    detailsForm.appendChild(nameBox);
+    detailsForm.appendChild(ageBox);
+}
