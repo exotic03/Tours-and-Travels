ca16b8d43630daf4dc758d27407362bce47910c0
Sourav Saha <saha83787@gmail.com>
2023-10-16 21:28:21 +0530

    admin page navbar, sidebar, footer refreshed

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -1,16 +1,5 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fa-solid fa-screwdriver-wrench"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
                 <a class="nav-link" href="index.php">
