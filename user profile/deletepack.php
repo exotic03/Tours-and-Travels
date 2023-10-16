d5a1970e6c8f6f8d127c2f017bd0e3896b126f13
Sourav Saha <saha83787@gmail.com>
2023-10-16 21:33:22 +0530

    package delete redirect updated

--- user profile/deletepack.php
+++ user profile/deletepack.php
@@ -3,5 +3,5 @@ $connect=mysqli_connect("localhost","root","","tour_and_travels");
 $id=$_GET['pack_id'];
 $del="DELETE FROM bookings_info WHERE id='$id'";
 $connect->query($del);
-header("location:userpackage.php");
+header("location:profile.php");
 ?>
