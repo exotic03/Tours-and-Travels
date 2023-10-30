f5797888f5600e168b087c6e9d52e0f58d88bc2f
Sourav Saha <saha83787@gmail.com>
2023-10-30 14:16:24 +0530

    payment gateway intregated

--- order_details.php
+++ order_details.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 $connect=mysqli_connect("localhost","root","","tour_and_travels");
-$id=$_GET["id"];
+$paymentid=$_POST['payment_id'];
+$id=$_POST["package_id"];
 echo $id;
 $user=$_SESSION['userid'];
 $sel="SELECT * FROM user_table WHERE id='$user'";
@@ -20,7 +21,13 @@ $contact_no=$_SESSION['temp_data']['contact'];
 $tname=$_SESSION['temp_data']['name'];
 $tage=$_SESSION['temp_data']['age'];
 $total_price=$package_price*$tquantity;
-$insert="INSERT INTO bookings_info SET package_name='$package_name',package_price='$total_price',tourist_number='$tquantity',date='$tdate',contact_no='$contact_no',traveller_name='$tname',traveller_age='$tage',user_id='$userid'";
-$connect->query($insert);
-header("location:home.php");
+$insert="INSERT INTO bookings_info SET package_name='$package_name',package_price='$total_price',tourist_number='$tquantity',date='$tdate',contact_no='$contact_no',traveller_name='$tname',traveller_age='$tage',user_id='$userid',payment_id='$paymentid'";
+$succres=$connect->query($insert);
+if($succres){
+    echo 'done';
+}
+else{
+    echo "Error: " . $sql . "<br>" . $connect->error;
+}
+// header("location:home.php");
 ?>
--- reviewbookingpage.php
+++ reviewbookingpage.php
@@ -60,15 +60,60 @@ if($_SESSION['userid']=="" || !isset($_SESSION['userid'])) {
                     <p>₹ <?php echo $total_price?> /-</p>
                 </div>
             </div>
-            <a href="order_details.php?id=<?php echo $data['id'];?>" id="bookpck">Book Package</a>
+            <a href="javascript:void(0)" data-packageid="<?php echo $data['id']; ?>" data-packagename="<?php echo $data['package_name']; ?>" data-amount="<?php echo $total_price ?>" id="bookpck">Book Package</a>
         </div>
     </div>
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/e04331d407.js" crossorigin="anonymous"></script>
+    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script>
+
+    $("#bookpck").click(function(){
+        var amount=$(this).attr('data-amount');
+        var packageName=$(this).attr('data-packagename');
+        var packageId=$(this).attr('data-packageid');
+
+var options = {
+    "key": "rzp_test_Gl1fZKrWBy2OPS", // Enter the Key ID generated from the Dashboard
+    "amount": amount*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Trippy",
+    "description": packageName,
+    "image": "https://example.com/your_logo",
+    "handler": function (response){
+        var paymentId=response.razorpay_payment_id;
+
+        $.ajax({
+            url:"order_details.php",
+            type:"POST",
+            data:{
+                package_id:packageId,
+                payment_id:paymentId
+            },
+            success:function(finalResponse){
+                if(finalResponse=='done'){
+                    window.location.href="http://localhost/Tours_and_Travels/home.php";
+                }
+                // else{
+                //     alert('Please check console to find error');
+                //     console.log(finalResponse);
+                // }
+            }
+        })
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+var rzp1 = new Razorpay(options);
+rzp1.open();
+e.preventDefault();
+})
+</script>
 </body>
 
 </html>
